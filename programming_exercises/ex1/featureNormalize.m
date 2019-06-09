396c12d26886cc4c84842c2e306a4ef97788ae4f
joonsun.baek@navercorp.com <joonsun.baek@navercorp.com>
2019-06-10 00:02:55 +0900

    ex1 extra excercise 푸는중

--- programming_exercises/ex1/featureNormalize.m
+++ programming_exercises/ex1/featureNormalize.m
@@ -26,10 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
+	mu = mean(X)
+	sigma =  max(X) - min(X)
 
-
-
-
+	X_norm = (X-mu) / sigma
 
 
 
