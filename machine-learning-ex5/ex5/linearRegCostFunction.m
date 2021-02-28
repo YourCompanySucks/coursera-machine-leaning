665e53f468e4dfd0f04620b69caeffc59699c01f
hwiho-park <hwiho.park@navercorp.com>
2021-02-28 14:24:50 +0900

    [#11] linearRegCostFunction 구현

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -19,15 +19,12 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+h = X*theta ;
+J = 1/(2*m) * sum((h-y).^2) - lambda/(2*m) * (theta' * theta) ;
 
-
-
-
-
-
-
-
-
+temp = theta ;
+temp(1) = 0 ;
+grad = 1/m * X'*(h-y) + lambda/m .* temp ;
 
 
 % =========================================================================
